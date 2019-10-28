32fe47b508230eba47278bfea3217ebf76092334
Matthew Bruns <matt.ubercomputer@gmail.com>
2019-10-27 17:32:19 -0700

    update contact phone number

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ function contact(props) {
         <div className={classes.OrderSuccess}>
             <h1 className={classes.Header}>Our Contact Info</h1>
             <p className={classes.Text}>matt.ubercomputer@gmail.com</p>
-            <p className={classes.Text}>555-555-5555</p>
+            <p className={classes.Text}>(971) 336-6601</p>
             <p className={classes.Signature}>— Matt and Mick</p>
         </div>
     );
