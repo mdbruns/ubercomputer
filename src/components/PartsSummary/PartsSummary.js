643b9a220ba26e828d5d21f96b5ab77497f125dd
Matthew Bruns <matt.ubercomputer@gmail.com>
2019-10-27 20:59:15 -0700

    update PartsSummary code to reflect whether video card selection is sli/crossfire

--- src/components/PartsSummary/PartsSummary.js
+++ src/components/PartsSummary/PartsSummary.js
@@ -10,9 +10,20 @@ function partsSummary(props) {
 
     if (props.history.location.pathname === "/checkout") {
         for (let category in props.selectedParts) {
+            let partNameDisplay = props.selectedParts[category].name;
+
+            if (category === "videoCard") {
+                if (props.isCrossfireSLICompatibleSystem) {
+                    partNameDisplay += " (2x Dual Video Cards)";
+                }
+                else {
+                    partNameDisplay += " (Single Video Card)";
+                }
+            }
+
             summaryContent.push(
                 <div key={shortid.generate()} className={classes.SummaryRow}>
-                    <p className={classes.Text}><span className={classes.Category}>{props.headings[category]}</span> : <span>{props.selectedParts[category].name}</span></p>
+                    <p className={classes.Text}><span className={classes.Category}>{props.headings[category]}</span> : <span>{partNameDisplay}</span></p>
                     <p className={classes.Price}>${category === "videoCard" && props.isCrossfireSLICompatibleSystem ? props.selectedParts[category].price * 2 : props.selectedParts[category].price}</p>
                 </div>
             );
@@ -20,9 +31,24 @@ function partsSummary(props) {
     }
     else {
         for (let category in props.parts) {
+            let partNameDisplay = "None";
+            
+            if (props.parts[category]) {
+                partNameDisplay = props.parts[category].name;
+            }
+
+            if (category === "videoCard") {
+                if (props.isCrossfireSLICompatibleSystem) {
+                    partNameDisplay += " (2x Dual Video Cards)";
+                }
+                else {
+                    partNameDisplay += " (Single Video Card)";
+                }
+            }
+
             summaryContent.push(
                 <div key={shortid.generate()} className={classes.SummaryRow}>
-                    <p className={classes.Text}><span className={classes.Category}>{props.headings[category]}</span> : <span>{props.parts[category] ? props.parts[category].name : "None"}</span></p>
+                    <p className={classes.Text}><span className={classes.Category}>{props.headings[category]}</span> : <span>{partNameDisplay}</span></p>
                     <p className={classes.Price}>${props.parts[category] ? props.parts[category].price : 0}</p>
                 </div>
             );
